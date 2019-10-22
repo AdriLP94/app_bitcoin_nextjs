cc776595df7126bc89a291711f96219c94a403a7
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-22 19:22:50 +0200

    Update Index.js

--- pages/Index.js
+++ pages/Index.js
@@ -1,8 +1,38 @@
+import MasterPage from './MasterPage';
+import Precio from '../components/Precio';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => {
+const Index = (props) => {
     return (
-        <p>Hola Mundo!</p>
+        <MasterPage>
+            <div className="row">
+                <div className="col-12">
+                    <Precio precio={props.precio}/>
+                </div>
+                <div className="col-md-8">
+                    <h2>Noticias</h2>
+                </div>
+                <div className="col-md-4">
+                    <h2>Proximos eventos</h2>
+                </div>
+            </div>
+        </MasterPage>
     );
 };
+
+Index.getInitialProps = async () => {
+    const precio = await fetch ('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=EUR');
+    const resPrecio = await precio.json();
+    
+    return {
+        precio : {
+            precioBitcoinActual : resPrecio.RAW.BTC.EUR.PRICE,
+            precioBitcoinAlto : resPrecio.RAW.BTC.EUR.HIGHDAY,
+            precioBitcoinBajo : resPrecio.RAW.BTC.EUR.LOWDAY,
+            cambioBitcoinHora : resPrecio.RAW.BTC.EUR.CHANGEPCTHOUR,
+            cambioBitcoinDia : resPrecio.RAW.BTC.EUR.CHANGEPCTDAY
+        }
+    };
+}
 
 export default Index;
