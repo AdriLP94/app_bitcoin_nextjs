4428608fc93b102cbc70a6556c54eb5152c27616
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-28 17:00:11 +0100

    Create Eventos.js

---
+++ components/Eventos.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Evento from './Evento';
+
+const Eventos = ({eventos}) => {
+
+    const eventosId = Object.keys(eventos);
+
+    return (
+        <div className="list-group">
+            { eventosId.map(key => (
+                <Evento
+                key={key}
+                info={eventos[key]} />
+            ))
+                
+            }
+        </div>
+    );
+};
+
+export default Eventos;
